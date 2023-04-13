e9b5a28707490fac25857fd264f995b03a7e0520
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-13 09:41:50 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "lesson-plan-39895",
   url: "https://lesson-plan-39895.botics.co",
   api: "https://lesson-plan-39895.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
